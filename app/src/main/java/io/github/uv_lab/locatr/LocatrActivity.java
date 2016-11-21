4709f2cff992d65852a4caa4ddd0c13bd1434d74
Wayne.You <lokiwuxi@gmail.com>
2016-11-21 13:31:17 +0800

    project init

---
+++ app/src/main/java/io/github/uv_lab/locatr/LocatrActivity.java
@@ -0,0 +1,13 @@
+package io.github.uv_lab.locatr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocatrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locatr);
+    }
+}
